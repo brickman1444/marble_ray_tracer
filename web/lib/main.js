ecc5d9a359a7c7e1d192506502ee19ba44f600c5
Zac Gross <brickman1444@gmail.com>
2020-05-25 11:19:21 -0500

    I have no idea what I was trying to do with this commit. These files have been sitting unstaged for months. I'm commiting them to this branch in case something important was in there.

--- web/lib/main.js
+++ web/lib/main.js
@@ -89,6 +89,67 @@ const getScene = () => {
     };
 };
 
+// Get 2d drawing context
+const gl = document.getElementById('canvas').getContext('webgl2');
+gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+const vertexCount = 6;
+const vertexLocations = [
+  // X, Y
+  -1.0, -1.0,
+   1.0, -1.0,
+  -1.0,  1.0,
+  -1.0,  1.0,
+   1.0, -1.0,
+   1.0,  1.0
+];
+
+gl.bufferData(
+  gl.ARRAY_BUFFER,
+  new Float32Array(vertexLocations),
+  gl.STATIC_DRAW
+);
+
+const program = gl.createProgram();
+const buildShader = (type, source) => {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  gl.attachShader(program, shader);
+};
+buildShader(
+  gl.VERTEX_SHADER,
+  `
+attribute vec2 a_position;
+void main() {
+  gl_Position = vec4(a_position, 0.0, 1.0);
+}`
+);
+
+buildShader(
+  gl.FRAGMENT_SHADER,
+  `
+void main() {
+  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}`
+);
+gl.linkProgram(program);
+gl.useProgram(program);
+const positionLocation = gl.getAttribLocation(program, 'a_position');
+gl.enableVertexAttribArray(positionLocation);
+const fieldCount = vertexLocations.length / vertexCount;
+gl.vertexAttribPointer(
+  positionLocation,
+  fieldCount,
+  gl.FLOAT,
+  gl.FALSE,
+  fieldCount * Float32Array.BYTES_PER_ELEMENT,
+  0
+);
+// Draw
+gl.drawArrays(gl.TRIANGLES, 0, vertexCount);
+
 const putData = (data) => {
     const ctx = document.getElementById('canvas').getContext('2d');
     ctx.putImageData(new ImageData(new Uint8ClampedArray(data), canvas.width, canvas.height), 0, 0);
@@ -107,19 +168,23 @@ const fps = new Fps(250,  document.querySelector('.fps'));
 
 const render = () => {
 
-    fps.tick();
+    gl.drawArrays(gl.TRIANGLES, 0, vertexCount);
 
-    const scene = getScene();
+    // fps.tick();
 
-    scene.objects[0].point.x = Math.sin(inc) * 3.0;
-    scene.objects[0].point.z = Math.cos(inc) * 3.0;
+    // const scene = getScene();
 
-    scene.objects[1].point.x = Math.sin(inc) * -3.0;
-    scene.objects[1].point.z = Math.cos(inc) * -3.0;
+    // scene.objects[0].point.x = Math.sin(inc) * 3.0;
+    // scene.objects[0].point.z = Math.cos(inc) * 3.0;
 
-    inc += parseFloat(0.06);
+    // scene.objects[1].point.x = Math.sin(inc) * -3.0;
+    // scene.objects[1].point.z = Math.cos(inc) * -3.0;
 
-    renderWasm(scene);
+    // inc += parseFloat(0.06);
+
+    // renderWasm(scene);
+
+    console.log("render")
 
     requestAnimationFrame(render);
 };
